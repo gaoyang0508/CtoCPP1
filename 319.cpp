fd7206e8ed081ec736d08e1a6a46d40f8c1bcdb2
gaoyang0508 <60972708+gaoyang0508@users.noreply.github.com>
2020-03-24 12:13:27 +0800

    Add files via upload

---
+++ 319.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream> 
+using namespace std;
+#include<iomanip>
+#define PI 3.1415926
+class cylinder
+{
+	private:		
+	double r,h;	
+	public:		
+	cylinder(double a,double b);	
+	~cylinder();	
+	double getvolumn();	
+	double getarea();	
+};
+cylinder::cylinder(double a,double b)
+{
+	h=a;
+	r=b;
+	cout<<"构造函数被调用" <<endl;
+}
+cylinder::~cylinder()
+{
+	cout<<"析构函数被调用"<<endl;
+}
+double cylinder::getvolumn()
+{
+	return(PI*r*r*h);
+}
+double cylinder::getarea()
+{
+	return(2*PI*r*r+2*r*PI*h);
+}
+int main()
+
+{
+
+    double d,h;
+
+    cin>>d>>h;
+
+    cylinder can(h,d/2);
+
+    cout<<fixed<<setprecision(6);
+
+    cout<<"油桶的容积是"<<can.getvolumn()<<" "<<endl;
+
+    cout<<"铁皮的面积是"<<can.getarea()<<endl;
+
+}
